e0c21c6d632db217d0d27cd68d6210b005db2339
LUIS FELIPE MARTINS PEREIRA <luis.fmpereira3@senacsp.edu.br>
2023-11-21 22:22:43 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './pages/Header';
 import DocPed from './pages/DocPed';
 import Kennel from './pages/Kennel';
+import Formulario from './pages/Formulario';
 function App() {
   return (
     <>
     <Header/>
     <DocPed/>
     <Kennel/>
+    <Formulario/>
     </>
 
   );
